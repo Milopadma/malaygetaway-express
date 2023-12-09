4aed57e6a9f74b99d88c6e5917ad269aa83aa256
mylo <milopadma@yahoo.com>
2023-12-09 12:54:15 +0800

    express

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const express = require('express')
-const app = express()
-const port = 3000
+app.get("/", (req: any, res: { send: (arg0: string) => void }) => {
+  res.send("Hello World!");
+});
 
-app.get('/', (req: any, res: { send: (arg0: string) => void }) => {
-  res.send('Hello World!')
-})
+app.post(
+  "/merchant/register",
+  (req: any, res: { send: (arg0: string) => void }) => {
+    console.log(req.body);
+    res.send("Hello World!");
+  }
+);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
