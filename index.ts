2263db7ccf06c9d8941f91bc19981ce1f5849902
mylo <milopadma@yahoo.com>
2023-12-11 15:06:17 +0800

    doc storage and email sending

--- index.ts
+++ index.ts
@@ -2,6 +2,10 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import { Merchant, Business } from "./types"; // Import your types
 import mongoose from "mongoose";
+import { Resend } from "resend";
+import { UTApi } from "uploadthing/server";
+import multer from "multer";
+import fs from "fs";
 
 // setup
 const app = express();
@@ -20,6 +24,18 @@ mongoose
   .catch((err) => {
     console.error(`MongoDB connection error: ${err}`);
   });
+const utapi = new UTApi({
+  apiKey: process.env.UPLOADTHING_SECRET,
+});
+
+if (!process.env.UPLOADTHING_SECRET) {
+  console.error("Uploadthing secret not found");
+  process.exit(1);
+}
+
+if (utapi) {
+  console.log("UTApi connected");
+}
 
 // schema def
 const merchantSchema = new mongoose.Schema({
@@ -41,6 +57,8 @@ const businessSchema = new mongoose.Schema({
 
 const Merchant = mongoose.model("Merchant", merchantSchema);
 const Business = mongoose.model("Business", businessSchema);
+
+// ----------------------------------------------------------------------
 
 // login endpoint
 app.post("/login", (req: Request, res: Response) => {
@@ -77,9 +95,65 @@ app.post("/merchant/register", (req: Request, res: Response) => {
       console.error(err);
     });
 
+  sendEmail();
+
   res.json({ message: "Merchant/register: Data Received" });
 });
+
+// send files endpoint
+const upload = multer();
+
+app.post("/sendFiles", upload.array("files"), async (req, res) => {
+  if (!req.files) {
+    return res.status(400).json({ message: "No files uploaded" });
+  }
+
+  const files = Array.from(Object.values(req.files)); // Convert files to an array
+
+  const formData = new FormData();
+
+  console.log(files);
+
+  files.forEach((file) => {
+    formData.append("files", file.buffer, file.originalname);
+  });
+
+  try {
+    const response = await utapi.uploadFiles(formData);
+    res.json({ message: "sendFiles: Data Received", response });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error sending files" });
+  }
+});
+
+// ----------------------------------------------------------------------
 
 app.listen(3000, () => {
   console.log(`Example app listening on port 3000`);
 });
+
+// functions
+function sendEmail() {
+  const resend = new Resend("re_R8dVwJbN_Jz2neHkTJJaL3wkD6AC9dXUH");
+
+  const data = {
+    name: "Milo",
+  };
+
+  resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: "milopadma@yahoo.com",
+    subject: "Hello World",
+    html: `<p>Congrats on sending your <strong>first email ${data.name}</strong>!</p>`,
+  });
+
+  console.log("sendEmail");
+}
+
+async function sendFiles(formData: FormData) {
+  const files = formData.getAll("files");
+  console.log(files);
+  const response = await utapi.uploadFiles(files);
+  console.log(response);
+}
