6f594519d1e10545c329c0732a6572923eb50e92
mylo <milopadma@yahoo.com>
2023-12-18 18:05:52 +0800

    small oopsie

--- api/auth/auth.controller.ts
+++ api/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { User } from "../../types";
 import dotenv from "dotenv";
-import { Model } from "mongoose";
+import userModel from "../../model/users/user.model";
 
 const {
   sendInternalError,
@@ -11,18 +11,13 @@ const {
 dotenv.config();
 
 export class AuthController {
-    private userModel: Model<User>; // type generic defaults to EMPTY if none passed in
-    constructor(userModel: Model<User>) {
-        this.userModel = userModel;
-    }
-
     async login(req: Request<User>, res: Response) {
         console.log("console.log", req.body);
         try {
             const { email, password } = req.body;
 
             // Check if the email and password combination exists in the database
-            const user = await this.userModel.findOne({ email, password });
+            const user = await userModel.findOne({ email, password });
 
             if (user) {
                 // Generate JWT token
